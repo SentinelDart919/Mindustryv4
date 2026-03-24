da3c0ce539b42ff2ac6a0ade0741f72fd9ed4ac3
SentinelDart919 <sentineldart5@gmail.com>
2026-03-24 23:45:45 +0100

    you can place any block under size 10 now

--- core/src/io/anuke/mindustry/content/blocks/Blocks.java
+++ core/src/io/anuke/mindustry/content/blocks/Blocks.java
@@ -47,7 +47,7 @@ public class Blocks extends BlockList implements ContentList{
 
         //Registers build blocks from size 1-6
         //no reference is needed here since they can be looked up by name later
-        for(int i = 1; i <= 6; i++){
+        for(int i = 1; i <= 10; i++){
             new BuildBlock("build" + i);
         }
 
