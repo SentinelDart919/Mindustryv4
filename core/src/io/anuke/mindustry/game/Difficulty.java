4bbe166e4086ee85ebeb0368acfbe1e83b21b1d4
SentinelDart919 <sentineldart5@gmail.com>
2026-03-24 16:17:58 +0100

    fixed Difficulty bug, in mayor difficulty more enemies

--- core/src/io/anuke/mindustry/game/Difficulty.java
+++ core/src/io/anuke/mindustry/game/Difficulty.java
@@ -3,11 +3,11 @@ package io.anuke.mindustry.game;
 import io.anuke.ucore.util.Bundles;
 
 public enum Difficulty{
-    training(3f, 3f, 0.5f),
-    easy(1.4f, 1.5f,0.75f),
+    training(3f, 3f, 2.0f),
+    easy(1.4f, 1.5f,1.75f),
     normal(1f, 1f, 1f),
-    hard(0.5f, 0.75f, 1.5f),
-    insane(0.25f, 0.5f, 2f);
+    hard(0.5f, 0.75f, 0.5f),
+    insane(0.25f, 0.5f, 0.25f);
 
     /**Multiplier of the time between waves.*/
     public final float timeScaling;
