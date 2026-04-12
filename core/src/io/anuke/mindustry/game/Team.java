f8013200ba0e56da014642a8ca65390fab5ca235
SentinelDart919 <sentineldart5@gmail.com>
2026-04-12 17:24:22 +0200

    added team "themass"

--- core/src/io/anuke/mindustry/game/Team.java
+++ core/src/io/anuke/mindustry/game/Team.java
@@ -9,7 +9,8 @@ public enum Team{
     red(Color.valueOf("e84737")),
     green(Color.valueOf("1dc645")),
     purple(Color.valueOf("ba5bd9")),
-    orange(Color.valueOf("e8c66a"));
+    orange(Color.valueOf("e8c66a")),
+    themass(Color.valueOf("320303"));
 
     public final static Team[] all = values();
     public final Color color;
