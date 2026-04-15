92bb946619daaea7117d638e8fc6723e6ba4125f
SentinelDart919 <sentineldart5@gmail.com>
2026-04-15 05:03:22 +0200

    added biomass units and weapons, added living boolean if block or unit is living will generate a scorch that looks like blood biomass units instead of a powercell will have a heart (added cool beat animation)

---
+++ core/src/io/anuke/mindustry/entities/units/BiomassUnit.java
@@ -0,0 +1,32 @@
+package io.anuke.mindustry.entities.units;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import io.anuke.ucore.core.Timers;
+import io.anuke.ucore.graphics.Draw;
+import io.anuke.ucore.util.Mathf;
+
+public class BiomassUnit extends GroundUnit{
+
+    @Override
+    public void drawStats(){
+        float hf = healthf();
+        float frequency = 1f + (1f - hf) * 3f;
+        float amplitude = 0.1f + (1f - hf) * 0.15f;
+
+        float scale = 1f + Mathf.sin(Timers.time() * frequency, 2f, amplitude);
+
+        Draw.color(Color.BLACK, team.color, hf + Mathf.absin(Timers.time(), hf * 5f, 1f - hf));
+        Draw.alpha(hitTime);
+        Draw.rect(getPowerCellRegion(), x, y,
+                getPowerCellRegion().getRegionWidth() * scale,
+                getPowerCellRegion().getRegionHeight() * scale,
+                rotation - 90);
+        Draw.color();
+    }
+
+    @Override
+    public TextureRegion getPowerCellRegion(){
+        return Draw.region("biomass-heart");
+    }
+}
---
+++ core/src/io/anuke/mindustry/entities/units/types/BiomassArtillery.java
@@ -0,0 +1,68 @@
+package io.anuke.mindustry.entities.units.types;
+
+import com.badlogic.gdx.graphics.Color;
+import io.anuke.mindustry.entities.Units;
+import io.anuke.mindustry.entities.units.BiomassUnit;
+import io.anuke.mindustry.world.blocks.Floor;
+import io.anuke.ucore.graphics.Draw;
+import io.anuke.ucore.util.Angles;
+import io.anuke.ucore.util.Mathf;
+
+public class BiomassArtillery extends BiomassUnit{
+
+    @Override
+    public void draw(){
+        Draw.alpha(hitTime / hitDuration);
+
+        float ft = Mathf.sin(walkTime * type.speed * 5f, 6f, 2f);
+
+        Floor floor = getFloorOn();
+
+        if(floor.isLiquid){
+            Draw.tint(Color.WHITE, floor.liquidColor, 0.5f);
+        }
+
+        for(int i : Mathf.signs){
+            Draw.rect(type.legRegion,
+                    x + Angles.trnsx(baseRotation, ft * i),
+                    y + Angles.trnsy(baseRotation, ft * i),
+                    12f * i, 12f - Mathf.clamp(ft * i, 0, 2), baseRotation - 90);
+        }
+
+        if(floor.isLiquid){
+            Draw.tint(Color.WHITE, floor.liquidColor, drownTime * 0.4f);
+        }else{
+            Draw.tint(Color.WHITE);
+        }
+
+        Draw.rect(type.baseRegion, x, y, baseRotation - 90);
+
+        Draw.rect(type.region, x, y, rotation - 90);
+
+        float tra = rotation - 90, trY = -weapon.getRecoil(this, true);
+
+        for(int i : Mathf.signs){
+            if(!weapon.weaponMirror && i < 0) continue;
+            Draw.rect(weapon.equipRegion,
+                    x + Angles.trnsx(tra, 0, trY),
+                    y + Angles.trnsy(tra, 0, trY), rotation - 90);
+        }
+
+        drawItems();
+
+        Draw.alpha(1f);
+    }
+
+    @Override
+    protected void patrol(){
+        if(Units.invalidateTarget(target, this)){
+            super.patrol();
+        }
+    }
+    @Override
+    protected void moveToCore(){
+        if(Units.invalidateTarget(target, this)){
+            super.moveToCore();
+        }
+    }
+}
