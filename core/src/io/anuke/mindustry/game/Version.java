5fa5fa9c012c0b81cf4f31115a7f2bc73b10eb81
SentinelDart919 <sentineldart5@gmail.com>
2026-05-08 21:02:24 +0200

    version I always forgot to commit this

--- core/src/io/anuke/mindustry/game/Version.java
+++ core/src/io/anuke/mindustry/game/Version.java
@@ -16,7 +16,7 @@ public class Version{
     /**Number specifying the major version, e.g. '4'*/
     public static int number = 63;
     /**Build number, e.g. '43'. set to '-1' for custom builds.*/
-    public static int build = 7;
+    public static int build = 8;
     /**Revision number. Used for hotfixes. Does not affect server compatibility.*/
     public static int revision = 2;
 
