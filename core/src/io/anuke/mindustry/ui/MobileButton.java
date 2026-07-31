e4fa4930dbeb4983c61ea287adb12311dd8669c2
SentinelDart919 <sentineldart5@gmail.com>
2026-07-31 05:12:49 +0200

    added Ucore Setup to build.gradle and fragments fixes

--- core/src/io/anuke/mindustry/ui/MobileButton.java
+++ core/src/io/anuke/mindustry/ui/MobileButton.java
@@ -2,6 +2,8 @@ package io.anuke.mindustry.ui;
 
 import com.badlogic.gdx.utils.Align;
 import io.anuke.ucore.scene.ui.ImageButton;
+import io.anuke.ucore.scene.ui.Label;
+import io.anuke.ucore.scene.ui.layout.Unit;
 
 public class MobileButton extends ImageButton{
 
@@ -10,6 +12,8 @@ public class MobileButton extends ImageButton{
         resizeImage(isize);
         clicked(listener);
         row();
-        add(text).growX().wrap().center().get().setAlignment(Align.center, Align.center);
+        Label label = add(text).growX().wrap().center().get();
+        label.setAlignment(Align.center, Align.center);
+        label.setFontScale(0.85f / Unit.dp.scl(1f));
     }
 }
